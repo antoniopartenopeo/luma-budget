21a2c473b8bb706d9ab138bef6512635fab3d87c
Antonio Corno <acvisuals@MacBook-Air-di-Antonio.local>
2026-01-03 23:04:12 +0100

    chore(seed): remove orphan seed-data re-export

--- src/lib/seed-data.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @deprecated This file is deprecated. Please import INITIAL_SEED_TRANSACTIONS 
- * directly from '@/features/transactions/api/repository'.
- */
-
-export { INITIAL_SEED_TRANSACTIONS } from "@/features/transactions/api/repository"
